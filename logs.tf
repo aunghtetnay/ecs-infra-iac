2857621dfa6106c527bb960f8e2fc3bd59771f33
aunghtetnay <aunghtetnay.x@gmail.com>
2023-10-27 00:19:32 +0630

    new backend log group:

--- logs.tf
+++ logs.tf
@@ -10,7 +10,25 @@ resource "aws_cloudwatch_log_group" "myapp_log_group" {
   }
 }
 
+
+# for backend
+
+resource "aws_cloudwatch_log_group" "backend_log_group" {
+  name              = var.aws_cloudwatch_backend_log_group_name#"/ecs/myapp"
+  retention_in_days = 30
+
+  tags = {
+    Name = var.aws_cloudwatch_log_group_tag_name
+  }
+}
+
 resource "aws_cloudwatch_log_stream" "myapp_log_stream" {
   name           = var.aws_cloudwatch_log_stream_name #"my-log-stream"
   log_group_name = aws_cloudwatch_log_group.myapp_log_group.name
+}
+
+#for backend
+resource "aws_cloudwatch_log_stream" "backend_log_stream" {
+  name           = var.aws_cloudwatch_backend_log_stream_name #"my-log-stream"
+  log_group_name = aws_cloudwatch_log_group.backend_log_group.name
 }
