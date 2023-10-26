2e77bf97f2fe6bba266c1c004b2e19268a6edce4
aunghtetnay <aunghtetnay.x@gmail.com>
2023-10-27 00:18:57 +0630

    Added backend target group and sigle loadbalancer

--- alb.tf
+++ alb.tf
@@ -23,6 +23,25 @@ resource "aws_alb_target_group" "app" {
     unhealthy_threshold = var.health_unhealthy_threshold#"2"
   }
 }
+# for backend
+resource "aws_alb_target_group" "backend" {
+  name        =  var.aws_backend_target_name #"myapp-target-group"
+  port        = var.backend_port
+  protocol    = var.target_backend_portocol #"HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type =  var.backend_target_type #"ip"
+
+  health_check {
+    healthy_threshold   = var.health_healthy_threshold #"3"
+    interval            = var.health_interval #"30"
+    protocol            = var.health_protocol#"HTTP"
+    matcher             = var.health_matcher#"200"
+    timeout             = var.health_timeout#"3"
+    path                = var.health_check_path
+    unhealthy_threshold = var.health_unhealthy_threshold#"2"
+  }
+}
+
 
 # Redirect all traffic from the ALB to the target group
 resource "aws_alb_listener" "front_end" {
@@ -30,8 +49,34 @@ resource "aws_alb_listener" "front_end" {
   port              = var.app_port2
   protocol          = var.alb_listener_protocol #"HTTP"
 
-  default_action {
-    target_group_arn = aws_alb_target_group.app.id
-    type             = var.alb_listener_default_type#"forward"
+   default_action {
+    type = "forward"
+    forward {
+      target_group {
+        arn = aws_alb_target_group.app.id
+      }
+
+      target_group {
+        arn = aws_alb_target_group.backend.id
+      }
+
+      stickiness {
+        enabled  = true
+        duration = 28800
+      }
+    }
   }
 }
+
+# #for backend
+# resource "aws_alb_listener" "backend" {
+#   load_balancer_arn = aws_alb.main.id
+#   port              = var.app_port2
+#   protocol          = var.alb_listener_protocol #"HTTP"
+
+#   default_action {
+#     target_group_arn = aws_alb_target_group.backend.id
+#     type             = var.alb_listener_default_type#"forward"
+#   }
+# }
+
